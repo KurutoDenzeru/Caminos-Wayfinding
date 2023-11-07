2b8d258bb4781ebd4d48887756ece5afb2b812a6
KurutoDenzeru <kurtdenzel51@gmail.com>
2023-11-07 23:41:35 +0800

    astar

--- MainForm-Visitor.php
+++ MainForm-Visitor.php
@@ -7,85 +7,32 @@
     <title>ABES - Mapping System</title>
 
     <!-- Bootstrap CDN CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!-- Bootstrap CDN Javascript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-
-    <!-- Bootstrap CDN JS Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <!-- jQuery CDN Core -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>    
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="icon" type="text/css" href="assets/img/favicon.png">
-    <link rel="stylesheet" href="assets/css/Styles.css">
 </head>
 
-    <!-- Asynchronous Request Data Image -->
-    <script>
-        $(document).ready(function() {
-            $("#search").keyup(function() {
-                var searchText = $(this).val();
-                $.ajax({
-                    url: "MainFormSearch.php",
-                    type: "post",
-                    data: {searchText: searchText},
-                    success: function(response) {
-                        $("#image").attr("src", response);
-                    }
-                });
-            });
-        });
-    </script>   
-    <!-- Asynchronous Request Data Image -->
-
-
-    <!-- Asynchronous Request of Algorithm -->
-
-
-      <script>
-        $(document).ready(function() {
-          $('#searchMap').on('input', function() {
-            var searchText = $(this).val();
-
-            $.ajax({
-              url: 'searchMap.php',
-              type: 'POST',
-              data: { searchMap: searchMap },
-              success: function(response) {
-                $('#imageContainer').html(response);
-              }
-            });
-          });
-        });
-      </script>
-
-
-    <!-- Asyncrhonous Request of Algorithm -->
-
-
-    <!-- Asynchronous Request of Estimated Time of Arrival / ETA -->
-    <script>
-      function search() {
-        var searchValue = $('#searchInput').val();
-
-        // Make an AJAX request to a PHP script
-        $.ajax({
-          url: 'search.php',
-          type: 'POST',
-          data: { searchValue: searchValue },
-          success: function(response) {
-            $('#searchResult').html(response);
-          }
-        });
+<!-- Asynchronous Request of Estimated Time of Arrival / ETA -->
+<script>
+  function search() {
+    var searchValue = $('#searchInput').val();
+    // Make an AJAX request to a PHP script
+    $.ajax({
+      url: 'search.php',
+      type: 'POST',
+      data: { searchValue: searchValue },
+      success: function(response) {
+        $('#searchResult').html(response);
       }
-    </script> 
-    <!-- Asynchronous Request of Estimated Time of Arrival / ETA -->
+    });
+  }
+</script> 
 
 
 <body class="bg-gradient-primary" style="align-items: center;display: flex;height: 100vh;background: url(&quot;assets/img/ABES.jpg&quot;) center / cover no-repeat;">
@@ -98,33 +45,6 @@
                         <div class="row">
                             <div class="col-lg-4 col-xl-4 col-xxl-4">
                                 <div id="back" class="p-5">
-
-                                    <!-- Carousel Promotion -->
-                                    <div class="carousel slide" data-bs-ride="false" id="carousel-1">
-                                        <div class="carousel-inner">
-                                            <div class="carousel-item active">
-                                                <img class="w-100 d-block border-5" src="assets/img/331961607_517802583833552_3972991969740231441_n.jpg" alt="Slide Image">
-                                            </div>
-                                            
-                                            <div class="carousel-item"><img class="w-100 d-block" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" alt="Slide Image"></div>
-                                            
-                                            <div class="carousel-item"><img class="w-100 d-block" style="border-radius: 15px" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" alt="Slide Image"></div>
-
-                                        </div>
-
-                                        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
-                                        
-                                        <ol class="carousel-indicators">
-                                            
-                                            <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
-                                            
-                                            <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
-                                            
-                                            <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
-                                        </ol>
-
-                                    </div>
-                                    <!-- Carousel Promotion -->
 
                                     <!-- Greeting Remarks -->
                                     <div class="row" style="margin: 15px 0px 15px 0px;">
